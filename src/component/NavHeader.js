e10afe4a91b7d6a50b7566813766a67104fc3275
Chanwit21 <bom.suthi@gmail.com>
2021-10-16 12:10:13 +0700

    init Project axios localStorage routes,date:16-10-2021

---
+++ src/component/NavHeader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NavHeader() {
+  return <div></div>;
+}
+
+export default NavHeader;
---
+++ src/route/route.js
@@ -0,0 +1,16 @@
+const routesAll = {
+  GUEST: {
+    routes: [{ path: '/', component: '' }],
+    redirect: '/',
+  },
+  CUSTOMER: {
+    routes: [{ path: '/', component: '' }],
+    redirect: '/',
+  },
+  ADMIN: {
+    routes: [{ path: '/', component: '' }],
+    redirect: '/',
+  },
+};
+
+export default routesAll;
