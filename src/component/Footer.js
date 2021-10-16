6620b5e36ee5bfad6670e30a9d9ba425c743f959
Chanwit21 <bom.suthi@gmail.com>
2021-10-16 17:45:49 +0700

    test add and config

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Footer() {
-  return <div className='flex-grow-0 bg-dark text-light'>Footer</div>;
+  return <div className='flex-grow-0 bg-dark text-light'>Footer Test</div>;
 }
 
 export default Footer;
