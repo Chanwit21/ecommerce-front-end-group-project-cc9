c0b643ea64b4d2a5456eed5922856cc89a4ae272
Chanwit21 <bom.suthi@gmail.com>
2021-10-17 00:49:28 +0700

    Cart context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import routesAll from './route/route';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { useAuthContext } from './context/AuthContext';
-import NavHeader from './component/NavHeader';
-import Footer from './component/Footer';
+import AppContainer from './layout/AppContainer';
+import { CartContextProvider } from './context/CartContext';
 
 function App() {
   const {
@@ -11,18 +11,16 @@ function App() {
   const role = user ? user.role : 'GUEST';
   return (
     <BrowserRouter>
-      <div className='d-flex flex-column'>
-        <NavHeader />
-        <div className='flex-grow-1' style={{ minHeight: '100vh' }}>
+      <CartContextProvider>
+        <AppContainer>
           <Switch>
             {routesAll[role].routes.map((route, index) => {
               return <Route key={index} to={route.path} component={route.component} />;
             })}
             <Redirect to={routesAll[role].redirect} />
           </Switch>
-        </div>
-        <Footer />
-      </div>
+        </AppContainer>
+      </CartContextProvider>
     </BrowserRouter>
   );
 }
--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+const style = {
+  footerContainer: { width: '70%', height: '100%' },
+};
+
 function Footer() {
-  return <div className='flex-grow-0 bg-dark text-light'>Footer Test</div>;
+  return (
+    <div className='flex-grow-0 bg-dark text-light'>
+      <div className='container ' style={style.footerContainer}>
+        Footer Test
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
