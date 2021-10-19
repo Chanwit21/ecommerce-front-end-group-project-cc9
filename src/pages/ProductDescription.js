9fd568fa235cc802286ec9963a5d769f3258083a
Orating <p.thanawats@gmail.com>
2021-10-20 00:01:41 +0700

    bootstarpProblem

--- src/pages/ProductDescription.js
+++ src/pages/ProductDescription.js
@@ -3,47 +3,102 @@ import React from 'react'
 function ProductDescription() {
   return (
     <div className='productDescriptionPage'>
-      <div className="card p-4"> {/* product detial*/}
-        <span>Home / Shop / Cheek / Blush</span>
-        <div className="row">
-          <div className="col-2" style={{ backgroundColor: 'gray' }}>
+      <div className="card p-4" style={{ backgroundImage: `linear-gradient(180deg, #FEF3F5 13.02%, rgba(251, 243, 245, 0) 100%)` }}> {/* product detial*/}
+        <span className='fs-6'>Home / Shop / Cheek / Blush</span>
+        <div className="row my-5">
+          <div className="col-1" >
+            <img className='mb-2' style={{ width: '100%' }} src='https://cdn.shopify.com/s/files/1/0450/5069/1744/products/Porcelain_400x.jpg?v=1626851297' alt="" />
+            <img className='mb-2' style={{ width: '100%' }} src='https://cdn.shopify.com/s/files/1/0450/5069/1744/products/Porcelain_400x.jpg?v=1626851297' alt="" />
+            <img className='mb-2' style={{ width: '100%' }} src='https://cdn.shopify.com/s/files/1/0450/5069/1744/products/Porcelain_400x.jpg?v=1626851297' alt="" />
+            <img className='mb-2' style={{ width: '100%' }} src='https://cdn.shopify.com/s/files/1/0450/5069/1744/products/Porcelain_400x.jpg?v=1626851297' alt="" />
+            <img className='mb-2' style={{ width: '100%' }} src='https://cdn.shopify.com/s/files/1/0450/5069/1744/products/Porcelain_400x.jpg?v=1626851297' alt="" />
+          </div>
+          <div className="col-5" >
             <img style={{ width: '100%' }} src='https://cdn.shopify.com/s/files/1/0450/5069/1744/products/Porcelain_400x.jpg?v=1626851297' alt="" />
           </div>
-          <div className="col-5" style={{ backgroundColor: 'lightGray' }}>
-            <img style={{ width: '100%' }} src='https://cdn.shopify.com/s/files/1/0450/5069/1744/products/Porcelain_400x.jpg?v=1626851297' alt="" />
-          </div>
-          <div className="col-5" style={{ backgroundColor: 'lightBlue' }}>
-            <h6>Seal the Deal Concealer</h6>
-            <hr />
-            <i class="bi bi-star-fill"></i>
-            <i class="bi bi-star-fill"></i>
-            <i class="bi bi-star-fill"></i>
-            <i class="bi bi-star-fill"></i>
-            <i class="bi bi-star-half"></i>
-            <p>$PRICE</p>
+          <div className="col-6" >
+            <h5>Seal the Deal Concealer</h5>
+            <hr className='m-0' />
+            <div className='m-0'>
+              <i className="bi bi-star-fill"></i>
+              <i className="bi bi-star-fill"></i>
+              <i className="bi bi-star-fill"></i>
+              <i className="bi bi-star-fill"></i>
+              <i className="bi bi-star-half"></i>
+            </div>
+            <p className='mt-4 fw-bold'>$50</p>
             <p>Color Shade Name</p>
-            <div style={{ width: '50px', height: '50px', backgroundColor: 'lightBlue' }}></div>
-
+            <div className="d-flex">
+              <div className='me-2' style={{ width: '50px', height: '50px', backgroundColor: '#F6C5B1' }}></div>
+              <div className='me-2' style={{ width: '50px', height: '50px', backgroundColor: '#A85E5B' }}></div>
+              <div className='me-2' style={{ width: '50px', height: '50px', backgroundColor: '#703840' }}></div>
+            </div>
+            <div className='d-flex align-items-center my-4'>
+              <div className='border border-dark'>
+                <button className='btn'>-</button>
+                <span>1</span>
+                <button className='btn'>+</button>
+              </div>
+              <button className='btn btn-dark mx-2'>ADD TO CART</button>
+              <button className='btn border border-dark mx-2'><i className="bi bi-heart"></i></button>
+            </div>
           </div>
-
         </div>
       </div>
-      <div>
-        <div className="card m-4 p-4"> {/* product info*/}
+      <div className='p-4' style={{ backgroundColor: '#FEF6F8' }}>
+        <div className="card p-4"> {/* product info*/}
           <h5>PRODUCT INFO</h5>
           <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quo eius nostrum dicta assumenda dignissimos! Nulla eius dicta dolorem voluptate consequatur hic harum eaque sapiente, enim repudiandae magnam sed beatae aliquam cum ab aliquid sequi asperiores consequuntur aperiam. Hic, laborum! Nulla quo molestiae, esse eligendi et id quibusdam odit omnis!</span>
         </div>
-        <div className="card m-4 p-4"> {/* ingreduents*/}
+        <div className="card my-4 p-4"> {/* ingreduents*/}
           <h5>INGREDIENTS</h5>
           <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quo eius nostrum dicta assumenda dignissimos! Nulla eius dicta dolorem voluptate consequatur hic harum eaque sapiente, enim repudiandae magnam sed beatae aliquam cum ab aliquid sequi asperiores consequuntur aperiam. Hic, laborum! Nulla quo molestiae, esse eligendi et id quibusdam odit omnis!</span>
         </div>
-        <div className="card m-4 p-4"> {/*delivery*/}
+        <div className="card my-4 p-4"> {/*delivery*/}
           <h5>DELIVERY</h5>
           <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quo eius nostrum dicta assumenda dignissimos! Nulla eius dicta dolorem voluptate consequatur hic harum eaque sapiente, enim repudiandae magnam sed beatae aliquam cum ab aliquid sequi asperiores consequuntur aperiam. Hic, laborum! Nulla quo molestiae, esse eligendi et id quibusdam odit omnis!</span>
         </div>
-        <div className="card m-4 p-4"> {/* payment and security*/}
+        <div className="card my-4 p-4"> {/* payment and security*/}
           <h5>{`PAYMENT & SECURITY`}</h5>
           <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quo eius nostrum dicta assumenda dignissimos! Nulla eius dicta dolorem voluptate consequatur hic harum eaque sapiente, enim repudiandae magnam sed beatae aliquam cum ab aliquid sequi asperiores consequuntur aperiam. Hic, laborum! Nulla quo molestiae, esse eligendi et id quibusdam odit omnis!</span>
+        </div>
+      </div>
+      <div class="accordion" id="accordionPanelsStayOpenExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+              Accordion Item #1
+            </button>
+          </h2>
+          <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+            <div class="accordion-body">
+              <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+              Accordion Item #2
+            </button>
+          </h2>
+          <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+            <div class="accordion-body">
+              <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+              Accordion Item #3
+            </button>
+          </h2>
+          <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+            <div class="accordion-body">
+              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
         </div>
       </div>
     </div>
