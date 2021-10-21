63ac280e5289e70677c80b0bbd04b326c3e5e0a0
Orating <p.thanawats@gmail.com>
2021-10-21 14:20:28 +0700

    route3

--- src/route/route.js
+++ src/route/route.js
@@ -1,25 +1,32 @@
-import Home from "../pages/Home";
-import ProductDescription from "../pages/ProductDescription";
-//ssssss\\
-
-
+import Checkout from '../pages/Checkout';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import ProductDescription from '../pages/ProductDescription';
+import SignUp from '../pages/SignUp';
 
 const routesAll = {
   GUEST: {
-    routes: [{ path: '/', component: Home },
-    { path: '/productDescription/:productId', component: ProductDescription }
+    routes: [
+      { path: '/', component: Home },
+      { path: '/login', component: Login },
+      { path: '/sign_up', component: SignUp },
+      { path: '/checkout', component: Checkout },
+      { path: '/productDescription/:productId', component: ProductDescription },
     ],
     redirect: '/',
   },
   CUSTOMER: {
-    routes: [{ path: '/', component: Home },
-    { path: '/productDescription/:productId', component: ProductDescription }
+    routes: [
+      { path: '/', component: Home },
+      { path: '/checkout', component: Checkout },
+      { path: '/productDescription/:productId', component: ProductDescription },
     ],
     redirect: '/',
   },
   ADMIN: {
-    routes: [{ path: '/', component: Home },
-    { path: '/productDescription/:productId', component: ProductDescription }
+    routes: [
+      { path: '/', component: Home },
+      { path: '/productDescription/:productId', component: ProductDescription },
     ],
     redirect: '/',
   },
