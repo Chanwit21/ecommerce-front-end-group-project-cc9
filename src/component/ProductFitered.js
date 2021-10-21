9d91165967aa57be6a2c3585d2a8950c56319686
Chanwit21 <bom.suthi@gmail.com>
2021-10-21 21:29:22 +0700

    config all

--- src/component/ProductFitered.js
+++ src/component/ProductFitered.js
@@ -27,26 +27,24 @@ function ProductFiltered({ filtered }) {
         className='d-inline-flex justify-content-center align-items-center mb-5 '
       >
         {productBox.map((item) => (
-          <>
-            <div className='d-flex align-items-center flex-column p-3'>
-              <i
-                className='bi bi-suit-heart-fill align-self-end p-2'
-                style={{ position: 'absolute', cursor: 'pointer' }}
-              ></i>
-              <img style={{ width: '330px', height: '330px' }} src={item.picture} key={item.id} alt='test' />
-              <h5 className='p-3'>{item.Name}</h5>
-              <div className='mt-1 mb-2'>
-                <i className='bi bi-star-fill'></i>
-                <i className='bi bi-star-fill'></i>
-                <i className='bi bi-star-fill'></i>
-                <i className='bi bi-star-half'></i>
-              </div>
-              <span className='mt-1 mb-1'>{item.Price}</span>
-              <button style={{ backgroundColor: 'white', cursor: 'pointer' }}>
-                <span>select shade</span>
-              </button>
+          <div key={item.id} className='d-flex align-items-center flex-column p-3'>
+            <i
+              className='bi bi-suit-heart-fill align-self-end p-2'
+              style={{ position: 'absolute', cursor: 'pointer' }}
+            ></i>
+            <img style={{ width: '330px', height: '330px' }} src={item.picture} key={item.id} alt='test' />
+            <h5 className='p-3'>{item.Name}</h5>
+            <div className='mt-1 mb-2'>
+              <i className='bi bi-star-fill'></i>
+              <i className='bi bi-star-fill'></i>
+              <i className='bi bi-star-fill'></i>
+              <i className='bi bi-star-half'></i>
             </div>
-          </>
+            <span className='mt-1 mb-1'>{item.Price}</span>
+            <button style={{ backgroundColor: 'white', cursor: 'pointer' }}>
+              <span>select shade</span>
+            </button>
+          </div>
         ))}
       </div>
     </>
