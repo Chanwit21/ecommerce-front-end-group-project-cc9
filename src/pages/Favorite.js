48b7bec7b51c609b89b4c06276ccf5e3e43f1bb8
NattakanSp <86395260+NattakanSp@users.noreply.github.com>
2021-10-22 14:39:43 +0700

    myAccount profile favorite

--- src/pages/Favorite.js
+++ src/pages/Favorite.js
@@ -7,20 +7,22 @@ function Favorite() {
   return (
     <>
       <AccountHeader />
-      <h4 className="container fw-bold d-flex justify-content-center mt-3 ">FAVORITE</h4>
-      <div className="container d-flex flex-row flex-wrap">
-        {orderItems.map((item) => (
-          <div className="col me-3" style={{ width: "290px", height: "495px" }}>
-            <ProductCardList
-              key={item.id}
-              name={item.name}
-              imageUrl={item.imageUrl}
-              price={item.price}
-              width="290px"
-              height="330px"
-            />
-          </div>
-        ))}
+      <div className="container ">
+        <h4 className=" fw-bold d-flex justify-content-center mt-3 ">FAVORITE</h4>
+        <div className=" d-flex row flex-wrap">
+          {orderItems.map((item) => (
+            <div className="col-3">
+              <ProductCardList
+                key={item.id}
+                name={item.name}
+                imageUrl={item.imageUrl}
+                price={item.price}
+                width="290px"
+                height="330px"
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
