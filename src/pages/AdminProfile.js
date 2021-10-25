7ec8ccadb3b50247135fa6bcf3bf52a5a1b2bbe7
Ploy Traisuth <ploytraisuth@Ploys-MBP.local>
2021-10-25 11:18:21 +0700

    add admin header

---
+++ src/pages/AdminProfile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AdminHeader from "../component/AdminHeader";
+import Profile from "../component/MyProfile/Profile";
+
+function AdminProfile() {
+  return (
+    <div>
+      <AdminHeader />
+      <Profile />
+    </div>
+  );
+}
+
+export default AdminProfile;
