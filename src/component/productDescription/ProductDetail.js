dd6b3fed8863fba3b1d2017c9ec0fec153d8d9c7
Orating <p.thanawats@gmail.com>
2021-10-26 15:54:42 +0700

    solve product

--- src/component/productDescription/ProductDetail.js
+++ src/component/productDescription/ProductDetail.js
@@ -5,6 +5,7 @@ function ProductDetail({ product, IsFavorite, productImage }) {
   const [numberOfProduct, setNumberOfProduct] = useState(1);
   const [checkFavorite, setCheckFavorite] = useState(IsFavorite); //IsFavorite from database
   const [selectedColor, setSelectedColor] = useState(product?.[0]?.color);
+  const [selectedImg, setSelectedImg] = useState('')
 
   useEffect(() => {
     setSelectedColor(product?.[0]?.color);
@@ -16,13 +17,13 @@ function ProductDetail({ product, IsFavorite, productImage }) {
 
   //* check color idx
 
-  const idx = product.findIndex((item) => item.color === selectedColor);
-  const productId = product[idx]?.id;
+  let imageIdx = productImage.findIndex((item => item.id === selectedImg))
+  if (imageIdx === -1) imageIdx = 0;
 
   const handleClickFavorite = async () => {
     if (!checkFavorite) {
       setCheckFavorite(true);
-      await axios.post('/product/favorite', { productId: product[0].id, productName: product[0].name });
+      await axios.post('/product/favorite', { productId: productImage[imageIdx].Product.id, productName: productImage[imageIdx].Product.name });
     }
     if (checkFavorite) {
       await axios.delete(`/product/favorite/${product[0].id}`);
@@ -31,7 +32,7 @@ function ProductDetail({ product, IsFavorite, productImage }) {
   };
 
   const handleAddToCart = async () => {
-    await axios.post('/product/cart', { quality: numberOfProduct, productId: product[idx].id });
+    await axios.post('/product/cart', { quality: numberOfProduct, productId: productImage[imageIdx].Product.id });
     alert('add to cart successful');
   };
 
@@ -47,35 +48,38 @@ function ProductDetail({ product, IsFavorite, productImage }) {
           <div className='row my-5'>
             <div className='col-1'>
               {productImage.map((item, index) => (
-                <img
-                  className={`mb-2 ${index === idx ? 'border border-dark' : ''}`}
-                  style={{ width: '4vw', height: '4vw' }}
-                  src={item?.imageUrl}
-                  alt=''
-                  onClick={() => {
-                    setSelectedColor(item.Product.color);
-                  }}
-                />
+                <div className='mb-2'>
+                  <img
+                    className={`${index === imageIdx ? 'border border-dark' : ''}`}
+                    style={{ width: '10vw', height: '10vw' }}
+                    src={item?.imageUrl}
+                    alt=''
+                    onClick={() => {
+                      setSelectedImg(item.id);
+                      console.log('click', index)
+                    }}
+                  />
+                </div>
               ))}
             </div>
             <div className='col-5'>
               <img
                 className='border'
                 style={{ width: '33.6vw', height: '33.6vw' }}
-                src={productImage?.[idx]?.imageUrl}
+                src={productImage?.[imageIdx]?.imageUrl}
                 alt=''
               />
             </div>
             <div className='col-6 ms-5' style={{ width: '28.75vw' }}>
-              <h4 style={{ fontSize: '24px' }}>{product?.[0]?.name}</h4> {/*name*/}
+              <h4 style={{ fontSize: '24px' }}>{productImage?.[imageIdx]?.Product?.name}</h4> {/*name*/}
               <hr className='m-0' />
-              <p className='mt-4 fw-bold' style={{ fontSize: '20px' }}>{`$${product?.[idx]?.price}`}</p> {/*price*/}
-              <p style={{ fontSize: '18px' }}>{product[idx]?.colorName}</p> {/*colorName*/}
+              <p className='mt-4 fw-bold' style={{ fontSize: '20px' }}>{`$${productImage?.[imageIdx]?.Product?.price}`}</p> {/*price*/}
+              <p style={{ fontSize: '18px' }}>{productImage?.[imageIdx]?.Product?.colorName}</p> {/*colorName*/}
               <div className='d-flex'>
                 {product.map((item, index) => (
                   <div
-                    className={`me-2 ${index === idx ? 'border border-dark border-2' : ''}`}
-                    onClick={() => setSelectedColor(item?.color)}
+                    className={`me-2 ${item.color === productImage[imageIdx]?.Product?.color ? 'border border-dark border-2' : ''}`}
+                    onClick={() => setSelectedImg(productImage.find(itemP => itemP.Product.color === item.color).id)}
                     style={{
                       width: '40px',
                       height: '40px',
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Carousel from '../component/Carousel';
 import HomeQuote from '../component/HomeQuote';
 import ProductFiltered from '../component/ProductFitered';
 import ProductDetail from '../component/productDescription/ProductDetail';
+import axios from 'axios';
+import { useAuthContext } from '../context/AuthContext';
 
 function Home(filtered) {
-  // const { state: { user } } = useAuthContext()
-  // const [product, setProduct] = useState([]);
-  // const [IsFavorite, setIsFavorite] = useState(false);
-  // const [productImage, setProductImage] = useState([]);
+  const { state: { user } } = useAuthContext()
+  const [product, setProduct] = useState([]);
+  const [IsFavorite, setIsFavorite] = useState(false);
+  const [productImage, setProductImage] = useState([]);
 
 
-  // useEffect(() => {
-  //   const run = async () => {
-  //     const { data: { product, productImage } } = await axios.get(`/product/${'productName'}`) //input productName
-  //     if (user) {
-  //       const { data: { IsFavorite }} = await axios.post(`/product/checkFavorite`, { productName: product?.[0]?.name });
-  //       setIsFavorite(IsFavorite);
-  //     }
-  //     setProduct(product);
-  //     setProductImage(productImage);
-  //   };
-  //   run();
-  // }, []);
+  useEffect(() => {
+    const run = async () => {
+      try {
+        const { data: { product, productImage } } = await axios.get(`/product/newAvailable/newAvailable`) //input productName
+        console.log(`product`, product)
+        if (user && product.length) {
+          const { data: { IsFavorite } } = await axios.post(`/product/checkFavorite`, { productName: product?.[0]?.name });
+          console.log(`IsFavorite`, IsFavorite)
+          setIsFavorite(IsFavorite);
+        }
+        console.log(`productImage`, productImage)
+        setProduct(product);
+        setProductImage(productImage);
+      }
+      catch (err) {
+        console.log(err.message)
+      }
+    };
+    run();
+  }, []);
   return (
     <div>
       <Carousel />
       <ProductFiltered filtered='FEATURED PRODUCT' />
       <HomeQuote />
-      {/* <ProductDetail product={product} IsFavorite={IsFavorite} productImage={productImage}/> */}
+      {product.length && <ProductDetail product={product} IsFavorite={IsFavorite} productImage={productImage} />}
       <HomeQuote />
       <ProductFiltered filtered='BEST SELLERS' />
     </div>
