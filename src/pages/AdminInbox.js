6c1dbc5a8e39d9d319411e9eddb7eccaef5fc894
Orating <p.thanawats@gmail.com>
2021-10-27 15:29:34 +0700

    contact

--- src/pages/AdminInbox.js
+++ src/pages/AdminInbox.js
@@ -1,12 +1,29 @@
-import React from "react";
+import axios from "../config/axios";
+import React, { useEffect, useState } from "react";
 import AdminHeader from "../component/AdminHeader";
 import InboxMessage from "../component/AdminInbox/InboxMessage";
 
 function AdminInbox() {
+  const [onPage, setOnPage] = useState(1)
+  useEffect(() => {
+    const run = async () => {
+      const { data: { contactUs } } = await axios.get(`/contactUs?offset=${7 * (onPage - 1)}`)
+      console.log(`contactUs`, contactUs)
+    }
+    run()
+  }, [onPage])
   return (
     <>
       <AdminHeader />
+      <div className='my-5'></div>
       <InboxMessage />
+      <InboxMessage />
+      <InboxMessage />
+      <InboxMessage />
+      <InboxMessage />
+      <InboxMessage />
+      <InboxMessage />
+      <div className='my-5'></div>
     </>
   );
 }
