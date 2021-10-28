10efac1147926697427c054aa8d8267860358395
NattakanSp <86395260+NattakanSp@users.noreply.github.com>
2021-10-28 17:05:19 +0700

    shoppingCart and favorite display when it is empty

--- src/component/ProductCard/ProductCardList.js
+++ src/component/ProductCard/ProductCardList.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { formatePrice } from '../../service/IntlFormatter';
+import React from "react";
+import { Link } from "react-router-dom";
+import { formatePrice } from "../../service/IntlFormatter";
 
 function ProductCardList({ name, imageUrl, price, width, height, alt }) {
   return (
-    <Link to={`/productDescription/${name}`} className='nav-link'>
-      <div className='d-flex align-items-center flex-column p-3' style={{ color: '#000' }}>
-        <i className='bi bi-suit-heart-fill align-self-end p-2' style={{ position: 'absolute', cursor: 'pointer' }}></i>
-        <img style={{ width, height, objectFit: 'cover' }} src={imageUrl} alt={alt} />
-        <h5 className='p-3'>{name}</h5>
+    <Link to={`/productDescription/${name}`} className="nav-link">
+      <div className="d-flex align-items-center flex-column p-3" style={{ color: "#000" }}>
+        <img style={{ width, height, objectFit: "cover" }} src={imageUrl} alt={alt} />
+        <h5 className="p-3">{name}</h5>
         {/* <div className='mt-1 mb-2'>
           <i className='bi bi-star-fill'></i>
           <i className='bi bi-star-fill'></i>
           <i className='bi bi-star-fill'></i>
           <i className='bi bi-star-half'></i>
         </div> */}
-        <span className='mt-1 mb-1'>{formatePrice(price)}</span>
-        <button style={{ backgroundColor: 'white', cursor: 'pointer' }}>
+        <span className="mt-1 mb-1">{formatePrice(price)}</span>
+        <button style={{ backgroundColor: "white", cursor: "pointer" }}>
           <span>select shade</span>
         </button>
       </div>
--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import ShoppingCartRow from '../component/shoppingCart/ShoppingCartRow';
-import ShoppingCartTotal from '../component/shoppingCart/ShoppingCartTotal';
-import { useCartContext } from '../context/CartContext';
+import React from "react";
+import ShoppingCartRow from "../component/shoppingCart/ShoppingCartRow";
+import ShoppingCartTotal from "../component/shoppingCart/ShoppingCartTotal";
+import { useCartContext } from "../context/CartContext";
+import emptyShoppingCart from "../pic/icons/empty-cart-png-9.png";
 
 export default function ShoppingCart() {
   const {
@@ -13,38 +14,60 @@ export default function ShoppingCart() {
   }, 0);
   return (
     <>
-      <div className='d-flex justify-content-evenly ms-5'>
-        <div className='p-3 ps-0 me-0 mt-5 mb-5' style={{ width: '964px' }}>
-          <div className='row border-bottom pb-1'>
-            <div className='col-6'>
-              <h5>Product</h5>
-            </div>
-            <div className='col-2 text-center'>
-              <h5>QTY</h5>
-            </div>
-            <div className='col-2 text-center'>
-              <h5>Price</h5>
-            </div>
-            <div className='col-2 text-center'>
-              <div className='d-flex justify-content-center align-items-center' style={{ width: '85%' }}>
-                <h5>Total</h5>
+      {carts.length > 0 ? (
+        <>
+          <div className="d-flex justify-content-evenly ms-5">
+            <div className="p-3 ps-0 me-0 mt-5 mb-5" style={{ width: "964px" }}>
+              <div className="row border-bottom pb-1">
+                <div className="col-6">
+                  <h5>Product</h5>
+                </div>
+                <div className="col-2 text-center">
+                  <h5>QTY</h5>
+                </div>
+                <div className="col-2 text-center">
+                  <h5>Price</h5>
+                </div>
+                <div className="col-2 text-center">
+                  <div className="d-flex justify-content-center align-items-center" style={{ width: "85%" }}>
+                    <h5>Total</h5>
+                  </div>
+                </div>
               </div>
+
+              {carts.map((item) => (
+                <ShoppingCartRow
+                  key={item.id}
+                  imageUrl={item.imageUrl}
+                  name={item.name}
+                  price={item.price}
+                  color={item.colorName}
+                  quantity={item.quality}
+                  cartItem={item}
+                />
+              ))}
             </div>
+            <ShoppingCartTotal total_price={totalPrice} />
           </div>
-          {carts.map((item) => (
-            <ShoppingCartRow
-              key={item.id}
-              imageUrl={item.imageUrl}
-              name={item.name}
-              price={item.price}
-              color={item.colorName}
-              quantity={item.quality}
-              cartItem={item}
+        </>
+      ) : (
+        <div className="container d-flex justify-content-center">
+          <div>
+            <img
+              src={emptyShoppingCart}
+              alt="emptycart"
+              className="bi bi-cart-x bi-10x ms-3 mt-5  row"
+              style={{ width: "100px", height: "100px", cursor: "pointer", position: "relative", left: "45%" }}
             />
-          ))}
+            <h4 className=" mt-0 pt-0 fw-normal mt-3" style={{ position: "relative", left: "25%" }}>
+              Your cart is currently empty.
+            </h4>
+            <h5 className=" fw-light mt-3" style={{ position: "relative", left: "25%" }}>
+              Add something to make me happy
+            </h5>
+          </div>
         </div>
-        <ShoppingCartTotal total_price={totalPrice} />
-      </div>
+      )}
     </>
   );
 }
