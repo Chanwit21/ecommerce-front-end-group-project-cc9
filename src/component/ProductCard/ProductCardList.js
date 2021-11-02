bd37a6903076e12f55cb20074ddc8dc984c79339
Chanwit21 <bom.suthi@gmail.com>
2021-11-02 14:07:56 +0700

    forgotPassword Success,date:2-11-2021

--- src/component/ProductCard/ProductCardList.js
+++ src/component/ProductCard/ProductCardList.js
@@ -5,12 +5,18 @@ import { formatePrice } from '../../service/IntlFormatter';
 function ProductCardList({ name, imageUrl, price, width, height, alt }) {
   return (
     <Link to={`/productDescription/${name}`} className='nav-link'>
-      <div className='d-flex align-items-center flex-column p-3' style={{ color: '#000' }}>
-        <img style={{ width, height, objectFit: 'cover' }} src={imageUrl} alt={alt} />
-        <h5 className='p-3'>{name}</h5>
+      <div
+        className='d-flex align-items-center justify-content-between flex-column p-3'
+        style={{ color: '#000', height: '31vw' }}
+      >
+        <div>
+          <img style={{ width, height, objectFit: 'cover' }} src={imageUrl} alt={alt} />
+          <h5 className='p-3'>{name}</h5>
+        </div>
+
         <span className='mt-1 mb-1'>{formatePrice(price)}</span>
-        <button style={{ backgroundColor: 'white', cursor: 'pointer' }}>
-          <span>select shade</span>
+        <button className='btn btn-dark' style={{ cursor: 'pointer' }}>
+          <span>SELECT SHADE</span>
         </button>
       </div>
     </Link>
